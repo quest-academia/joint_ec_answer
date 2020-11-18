424b88d8e291f57b3e42437edff83047dd7f4259
Takano <anpanmannoatarashiikao@gmail.com>
2020-11-18 22:03:20 +0900

    add:トップページ作成

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,5 @@
 */
 
 
-Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('/');
-
-Route::resource('products', 'FrontProductsController', ['only' => ['index', 'show']])->middleware('auth');
-Route::get('search','FrontProductsController@search')->name('search')->middleware('auth');
-
-Route::resource('users', 'FrontUsersController', ['only' => ['show', 'edit', 'update', 'destroy']])->middleware('auth');
-
-Route::resource('carts', 'FrontCartController', ['only' => ['index', 'store', 'destroy']])->middleware('auth');
-
-Route::resource('orders', 'FrontOrdersController', ['only' => ['index', 'store', 'show', 'destroy']])->middleware('auth');
-
-Route::post('deleteOrder','FrontOrdersController@deleteOrder')->name('deleteOrder')->middleware('auth');
-Route::get('threemonthsIndex','FrontOrdersController@threemonthsIndex')->name('threemonthsIndex')->middleware('auth');
-Route::get('orderComplete','FrontordersController@orderComplete')->name('orderComplete')->middleware('auth');;
